7276e0403cdd58b53c5d1dbf818c07e9b2daac08
Euiyeon Kim <ppang327@gmail.com>
2019-10-28 09:05:28 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>@ibsheet/loader v{{loaderVersion}}</p>
+    <div class="main-navs">
+      <h3>Go to Pages:</h3>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/page1">Page1</router-link></li>
+        <li><router-link to="/page2">Page2</router-link></li>
+      </ul>  
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import loader from '@ibsheet/loader'
+import VueRouter from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Page1 from './components/Page1.vue'
+import Page2 from './components/Page2.vue'
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', name: 'home', component: HelloWorld },
+    { path: '/page1', name: 'page1', component: Page1 },
+    { path: '/page2', name: 'page2', component: Page2 }
+  ]
+})
+
+loader.config({
+  registry: [{
+    name: 'ibsheet',
+    // baseUrl: '/assets/ibsheet'
+    baseUrl: 'https://dev.ibleaders.com/lib/ibsheet-js/v8/core/nightly/latest'
+  }]
+})
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      loaderVersion: loader.version
+    }
+  },
+  router
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +58,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.main-navs {
+  > h3 {
+    margin: 40px 0 0;  
+  }
+  > ul {
+    list-style-type: none;
+    padding: 0;
+    > li {
+      display: inline-block;
+      margin: 0 10px;
+      a > {
+        color: #42b983;
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/Page1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>page1 works!</p>
+    <div id="sheetContainer"></div>
+  </div>
+</template>
+
+<script>
+import loader from '@ibsheet/loader'
+import { SheetSampleData } from '../shared/ibsheet-data'
+
+const SHEET_ID = 'sheet'
+
+export default {
+  mounted() {
+    const { data, options } = SheetSampleData[0]
+    loader.createSheet({
+      id: SHEET_ID,
+      el: 'sheetContainer',
+      options,
+      data
+    })
+  },
+  beforeDestroy() {
+    loader.removeSheet(SHEET_ID)
+  }
+}
+</script>
